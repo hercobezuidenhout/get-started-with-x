8f5b4ef720e061566042ef4952247b4bd198f445
Herco Bezuidenhout <hercobez@icloud.com>
2026-04-25 17:47:12 +0200

    feat: Create first model

--- database/migrations/2026_04_25_151449_create_chirps_table.php
+++ database/migrations/2026_04_25_151449_create_chirps_table.php
@@ -16,7 +16,7 @@ return new class extends Migration
             $table->foreignId('user_id')->nullable()->constrained()->cascadeOnDelete();
             $table->string('message', 255);
             $table->timestamps();
-        });
+        }); 
     }
 
     /**
